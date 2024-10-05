cf58f875c58a69d9b1d3d826decf1c342d7b816a
zentrader38 <zentrader38@gmail.com>
2024-10-05 20:45:10 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  server: {
-    port: 3000, // Change this to a different port
-  },
   build: {
     outDir: './docs'
   },
